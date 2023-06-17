b696c6c7f70bc571922fe6b3fc6aa61052a0b1db
mardochee01 <mardoche.gneran@gmail.com>
2023-06-17 00:08:00 +0000

    task OK

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Mardochée Gneran <https://github.com/mardochee01>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
